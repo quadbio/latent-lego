58f2b3a31c8acccd0d2d6d4b33d7983caeb95738
Jonas Simon Fleck <jonas.fleck@bsse.ethz.ch>
2020-10-27 14:03:34 +0100

    Restructuring model signature.

--- latent/flow/modules.py
+++ latent/flow/modules.py
@@ -22,6 +22,7 @@ class Encoder(Model):
         l1 = 0.0,
         l2 = 0.0,
         architecture = [128, 128],
+        build_model = False,
         **kwargs
     ):
         super().__init__(**kwargs)
@@ -34,49 +35,59 @@ class Encoder(Model):
         self.architecture =  architecture
         self.initializer = keras.initializers.glorot_normal()
 
-        self.input_layer = Input(shape=(self.x_dim, ), name='data')
-        inputs, outputs = self._build_model()
-        self.model = Model(inputs=inputs, outputs=outputs, name='encoder')
+        self._core()
+        self._final()
 
-    def _build_model(self):
-        '''Constructs the full model network'''
-        h = self.input_layer
+        if build_model:
+            self._build()
+
+    def call(self, inputs):
+        '''Full forward pass through model'''
+        h = inputs
+        for layer in self.core_stack:
+            h = layer(h)
+        h = self.final_layer(h)
+        outputs = self.final_act(h)
+        return outputs
+
+    def _core(self):
+        '''Core layers of the model'''
+        self.core_stack = []
         for idx, dim in enumerate(self.architecture):
             layer_name = f'encoder_{idx}'
-            h = Dense(
+            dense = Dense(
                 dim, name = layer_name,
                 kernel_initializer = self.initializer,
                 kernel_regularizer = l1_l2(self.l1, self.l2)
-            )(h)
+            )
+            self.core_stack.append(dense)
 
             if self.batchnorm:
-                h = BatchNormalization(center=True, scale=False)(h)
+                bn = BatchNormalization(center=True, scale=False)
+                self.core_stack.append(bn)
 
-            h = LeakyReLU()(h)
+            lrelu = LeakyReLU()
+            self.core_stack.append(lrelu)
 
             if self.dropout_rate > 0.0:
-                h = Dropout(self.dropout_rate)(h)
+                dout = Dropout(self.dropout_rate)
+                self.core_stack.append(dout)
 
-        inputs, outputs = self._build_final(h)
-        return inputs, outputs
-
-    def _build_final(self, inputs):
+    def _final(self):
         '''Final layer of the model'''
-        h = Dense(
-            self.latent_dim, name = 'encoder_build_final',
+        self.final_layer = Dense(
+            self.latent_dim, name = 'encoder_final',
             kernel_initializer = self.initializer,
             kernel_regularizer = l1_l2(self.l1, self.l2)
-        )(inputs)
-        outputs = Activation('linear', name='latent')(h)
-        return self.input_layer, outputs
+        )
+        self.final_act = Activation('linear', name='encoder_final_activation')
 
-    def call(self, inputs):
-        return self.model(inputs)
+    def _build(self):
+        self.build(input_shape=(self.x_dim, ))
 
 
 class Decoder(Model):
     '''Classical encoder model'''
-
     def __init__(
         self,
         x_dim,
@@ -86,6 +97,7 @@ class Decoder(Model):
         l1 = 0.0,
         l2 = 0.0,
         architecture = [128, 128],
+        build_model = False,
         **kwargs
     ):
         super().__init__(**kwargs)
@@ -98,44 +110,55 @@ class Decoder(Model):
         self.architecture =  architecture
         self.initializer = keras.initializers.glorot_normal()
 
-        self.input_layer = Input(shape=(self.latent_dim, ), name='latent')
-        inputs, outputs = self._build_model()
-        self.model = Model(inputs=inputs, outputs=outputs, name='encoder')
+        self._core()
+        self._final()
 
-    def _build_model(self):
-        '''Constructs the full model network'''
-        h = self.input_layer
+        if build_model:
+            self._build()
+
+    def call(self, inputs):
+        '''Full forward pass through model'''
+        h = inputs
+        for layer in self.core_stack:
+            h = layer(h)
+        h = self.final_layer(h)
+        outputs = self.final_act(h)
+        return outputs
+
+    def _core(self):
+        '''Core layers of the model'''
+        self.core_stack = []
         for idx, dim in enumerate(self.architecture):
             layer_name = f'decoder_{idx}'
-            h = Dense(
+            dense = Dense(
                 dim, name = layer_name,
                 kernel_initializer = self.initializer,
                 kernel_regularizer = l1_l2(self.l1, self.l2)
-            )(h)
+            )
+            self.core_stack.append(dense)
 
             if self.batchnorm:
-                h = BatchNormalization(center=True, scale=False)(h)
+                bn = BatchNormalization(center=True, scale=False)
+                self.core_stack.append(bn)
 
-            h = LeakyReLU()(h)
+            lrelu = LeakyReLU()
+            self.core_stack.append(lrelu)
 
             if self.dropout_rate > 0.0:
-                h = Dropout(self.dropout_rate)(h)
+                dout = Dropout(self.dropout_rate)
+                self.core_stack.append(dout)
 
-        inputs, outputs = self._build_final(h)
-        return inputs, outputs
-
-    def _build_final(self, inputs):
+    def _final(self):
         '''Final layer of the model'''
-        h = Dense(
-            self.x_dim, name = 'decoder_build_final',
+        self.final_layer = Dense(
+            self.x_dim, name = 'decoder_final',
             kernel_initializer = self.initializer,
             kernel_regularizer = l1_l2(self.l1, self.l2)
-        )(inputs)
-        outputs = Activation('linear', name='reconstruction_output')(h)
-        return self.input_layer, outputs
+        )
+        self.final_act = Activation('linear', name='reconstruction_output')
 
-    def call(self, inputs):
-        return self.model(inputs)
+    def _build(self):
+        self.build(input_shape=(self.latent_dim, ))
 
 
 class CountDecoder(Decoder):
@@ -148,16 +171,24 @@ class CountDecoder(Decoder):
         self.sf_layer = Input(shape=(1, ), name='size_factors')
         super().__init__(**kwargs)
 
-    def _build_final(self, inputs):
+    def call(self, inputs):
+        '''Full forward pass through model'''
+        h = inputs[0]
+        sf = inputs[1]
+        for layer in self.core_stack:
+            h = layer(h)
+        h = self.mean_layer(h)
+        outputs = self.norm_layer([h, sf])
+        return outputs
+
+    def _final(self, inputs):
         '''Final layer of the model'''
-        mean = Dense(
+        self.mean_layer = Dense(
             self.x_dim, name='mean',
             kernel_initializer = self.initializer,
             kernel_regularizer = l1_l2(self.l1, self.l2)
-        )(inputs)
-        outputs = ColwiseMult()([mean, self.sf_layer])
-
-        return [self.input_layer, self.sf_layer], outputs
+        )
+        self.norm_layer = ColwiseMult(name='reconstruction_output')
 
 
 class PoissonDecoder(CountDecoder):
@@ -169,7 +200,7 @@ class PoissonDecoder(CountDecoder):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    def _build_final(self, inputs):
+    def _final(self, inputs):
         '''Final layer of the model'''
         mean = Dense(
             self.x_dim, name='mean',
@@ -191,7 +222,7 @@ class NegativeBinomialDecoder(CountDecoder):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    def _build_final(self, inputs):
+    def _final(self, inputs):
         '''Final layer of the model'''
         mean = Dense(
             self.x_dim, name='mean',
