041c4aa688cdb19dd426365bd696338d126318a2
Jonas Simon Fleck <jonas.fleck@bsse.ethz.ch>
2020-10-28 15:54:37 +0100

    Various VAE implementations.

---
+++ latent/flow/vae.py
@@ -0,0 +1,48 @@
+'''Tensorflow Variational Autoencoder Models'''
+
+import tensorflow as tf
+import tensorflow.keras as keras
+from tensorflow.keras import backend as K
+from tensorflow.keras import Input, Model
+from tensorflow.keras.losses import MeanSquaredError, Poisson
+
+from .ae import Autoencoder, CountAutoencoder, PoissonAutoencoder
+from .ae import NegativeBinomialAutoencoder, ZINBAutoencoder
+from .encoder import VariationalEncoder
+
+
+class VariationalAutoencoder(Autoencoder):
+    def __init__(self, beta=0.1, **kwargs):
+        self.beta = beta
+        super().__init__(**kwargs)
+
+    def _encoder(self):
+        self.encoder = VariationalEncoder(
+            beta = self.beta,
+            latent_dim = self.latent_dim,
+            dropout_rate = self.dropout_rate,
+            batchnorm = self.batchnorm,
+            l1 = self.l1,
+            l2 = self.l2,
+            architecture = self.architecture
+        )
+
+
+class CountVAE(CountAutoencoder, VariationalAutoencoder):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+
+class PoissonVAE(PoissonAutoencoder, VariationalAutoencoder):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+
+class NegativeBinomialVAE(NegativeBinomialAutoencoder, VariationalAutoencoder):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+
+class ZINBVAE(ZINBAutoencoder, VariationalAutoencoder):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
