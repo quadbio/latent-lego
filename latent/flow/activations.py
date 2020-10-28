bf0214f23f2c3015b3c57272e938b2d06bea1cb8
Jonas Simon Fleck <jonas.fleck@bsse.ethz.ch>
2020-10-28 17:20:31 +0100

    Flexible activations.

--- latent/flow/activations.py
+++ latent/flow/activations.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from tensorflow.keras import backend as K
+from tensorflow.keras.layers import ReLU, LeakyReLU, PReLU, Activation
 
 
 def clipped_exp(x):
@@ -8,3 +9,14 @@ def clipped_exp(x):
 
 def clipped_softplus(x):
     return tf.clip_by_value(tf.nn.softplus(x), 1e-4, 1e4)
+
+
+ACTIVATIONS = {
+    'prelu': PReLU(),
+    'relu': ReLU(),
+    'leaky_relu': LeakyReLU(),
+    'linear': Activation('linear'),
+    'sigmoid': Activation('sigmoid'),
+    'clipped_exp': Activation(clipped_exp),
+    'clipped_softplus': Activation(clipped_softplus)
+}
