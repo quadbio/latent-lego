349d6be5de70aa5cfc04d85b0af60c0c693259c4
Jonas Simon Fleck <jonas.fleck@bsse.ethz.ch>
2020-10-29 13:53:50 +0100

    Refactoring DenseStack and DenseBlock.

--- latent/flow/core.py
+++ latent/flow/core.py
@@ -10,11 +10,54 @@ from tensorflow.keras.layers import BatchNormalization, Dense, Dropout
 from .activations import ACTIVATIONS
 
 
-class CoreNetwork(Model):
-    '''Core network of encoders and decoders'''
+class DenseBlock(Layer):
+    '''Basic dense layer block'''
     def __init__(
         self,
-        name = 'net',
+        name = 'dense_block',
+        dropout_rate = 0.1,
+        batchnorm = True,
+        l1 = 0.0,
+        l2 = 0.0,
+        activation = 'leaky_relu',
+        **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.name = name
+        self.dropout_rate = dropout_rate
+        self.batchnorm =  batchnorm
+        self.l1 = l1
+        self.l2 = l2
+        self.initializer = keras.initializers.glorot_normal()
+
+        # Define block components
+        self.dense = Dense(
+            dim, name = self.name,
+            kernel_initializer = self.initializer,
+            kernel_regularizer = l1_l2(self.l1, self.l2)
+        )
+        self.bn = BatchNormalization(center=True, scale=True)
+        if isinstance(activation, str):
+            self.activation = ACTIVATIONS.get(activation, LeakyReLU())
+        else:
+            self.activation = activation
+        self.dropout = Dropout(self.dropout_rate)
+
+
+    def call(self, inputs):
+        '''Full forward pass through model'''
+        h = self.dense(inputs)
+        h = self.bn(h)
+        h = self.activation(h)
+        outputs = self.dropout(h)
+        return outputs
+
+
+class DenseStack(Layer):
+    '''Core layer stack of encoders and decoders'''
+    def __init__(
+        self,
+        name = 'dense_stack',
         dropout_rate = 0.1,
         batchnorm = True,
         l1 = 0.0,
@@ -24,46 +67,24 @@ class CoreNetwork(Model):
         **kwargs
     ):
         super().__init__(name=name, **kwargs)
-        self.dropout_rate = dropout_rate
-        self.batchnorm =  batchnorm
-        self.l1 = l1
-        self.l2 = l2
         self.architecture =  architecture
-        self.initializer = keras.initializers.glorot_normal()
 
-        if isinstance(activation, str):
-            self.activation = ACTIVATIONS.get(activation, LeakyReLU())
-        else:
-            self.activation = activation
-
-        self._core()
+        self.layer_stack = []
+        for idx, dim in enumerate(self.architecture):
+            layer_name = f'd{self.name}_{idx}'
+            layer = DenseBlock(
+                name = layer_name,
+                dropout_rate = dropout_rate,
+                batchnorm = batchnorm,
+                l1 = l1,
+                l2 = l2
+            )
+            self.layer_stack.append(layer)
 
     def call(self, inputs):
         '''Full forward pass through model'''
         h = inputs
-        for layer in self.core_stack:
+        for layer in self.layer_stack:
             h = layer(h)
         outputs = h
         return outputs
-
-    def _core(self):
-        '''Core layer stack'''
-        self.core_stack = []
-        for idx, dim in enumerate(self.architecture):
-            layer_name = f'd{self.name}_{idx}'
-            layer = Dense(
-                dim, name = layer_name,
-                kernel_initializer = self.initializer,
-                kernel_regularizer = l1_l2(self.l1, self.l2)
-            )
-            self.core_stack.append(layer)
-
-            if self.batchnorm:
-                layer = BatchNormalization(center=True, scale=True)
-                self.core_stack.append(layer)
-
-            self.core_stack.append(self.activation)
-
-            if self.dropout_rate > 0.0:
-                layer = Dropout(self.dropout_rate)
-                self.core_stack.append(layer)
