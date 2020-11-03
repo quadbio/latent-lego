f734a510a2fad3f678dd9c7f8a69196bd79572b9
Jonas Simon Fleck <jonas.fleck@bsse.ethz.ch>
2020-11-03 14:00:47 +0100

    Full testing script for TWAE.

--- test_twae.py
+++ test_twae.py
@@ -7,6 +7,7 @@ warnings.filterwarnings('ignore')
 
 import numpy as np
 import scanpy as sc
+import anndata as ad
 
 from tensorflow.python.util import deprecation
 deprecation._PRINT_DEPRECATION_WARNINGS = False
@@ -16,7 +17,7 @@ from tensorflow.data import Dataset
 from tensorflow.keras.losses import MeanSquaredError, Poisson
 from keras.utils import plot_model
 
-from latent.flow.twae import MMDTwinAutoencoder
+from latent.flow.twae import TwinAutoencoder
 from latent.flow.ae import Autoencoder, PoissonAutoencoder
 from latent.flow.vae import NegativeBinomialVAE as NBVAE
 from latent.flow.ae import NegativeBinomialAutoencoder as NBAE
@@ -66,26 +67,26 @@ if __name__ == '__main__':
     rna_use = np.array(rna.X.todense())[:5000, :]
     atac_use = np.array(atac.X.todense())[:5000, :]
 
-    # n_umis = X_use.sum(1)
-    # size_factors = n_umis / np.median(n_umis)
+    n_umis = rna_use.sum(1)
+    rna_sf = n_umis / np.median(n_umis)
 
-    rna_ae = Autoencoder(
+    n_counts = atac_use.sum(1)
+    atac_sf = n_counts / np.median(n_counts)
+
+    rna_ae = NBAE(
         x_dim = rna_use.shape[1],
         activation = 'leaky_relu',
-        latent_dim = 10
+        latent_dim = 20
     )
-    atac_ae = Autoencoder(
+    atac_ae = NBAE(
         x_dim = atac_use.shape[1],
         activation = 'leaky_relu',
-        latent_dim = 10
+        latent_dim = 20
     )
-    twae = MMDTwinAutoencoder(
-        models = [rna_ae, atac_ae],
-        losses = [MeanSquaredError(), MeanSquaredError()]
-    )
+    twae = TwinAutoencoder([rna_ae, atac_ae])
     twae.compile()
     history = twae.fit(
-        [rna_use, atac_use],
+        [[rna_use, rna_sf], [atac_use, atac_sf]],
         batch_size = args.batch_size,
         epochs = args.epochs,
         use_multiprocessing = True,
@@ -93,47 +94,16 @@ if __name__ == '__main__':
     )
     history.history
 
-    rna_lat, atac_lat = twae.transform([rna_use, atac_use])
+    latent, labels = twae.transform([rna_use, atac_use])
 
-    adata.obsm['X_ae'] = latent
-    sc.pp.neighbors(adata, use_rep='X_ae', n_neighbors=30)
+    latad = ad.AnnData(X=latent)
+    latad.tech = labels
+
+    sc.pp.neighbors(adata, use_rep='X', n_neighbors=30)
     sc.tl.umap(adata, min_dist=0.1, spread=0.5)
 
-    p = sc.pl.scatter(adata, show=False, basis='ae', color='celltype')
+    p = sc.pl.scatter(adata, show=False, basis='X', color='tech')
     p.figure.savefig('latent_final.png')
 
-    p = sc.pl.scatter(adata, show=False, basis='umap', color='celltype')
+    p = sc.pl.scatter(adata, show=False, basis='umap', color='tech')
     p.figure.savefig('umap_final.png')
-
-
-    #### Custom model
-    # rna_enc = Encoder(
-    #     latent_dim = 10,
-    #     name = 'rna_encoder'
-    # )
-    # rna_dec = Decoder(
-    #     x_dim = rna_use.shape[1],
-    #     name = 'rna_decoder'
-    # )
-    # atac_enc = Encoder(
-    #     latent_dim = 10,
-    #     name = 'atac_encoder'
-    # )
-    # atac_dec = Decoder(
-    #     x_dim = atac_use.shape[1],
-    #     name = 'atac_decoder'
-    # )
-    #
-    # rna_input = layers.Input((rna_use.shape[1],))
-    # atac_input = layers.Input((atac_use.shape[1],))
-    #
-    # latent1 = rna_enc(rna_input)
-    # latent2 = atac_enc(atac_input)
-    #
-    # out1 = rna_dec(latent1)
-    # out2 = atac_dec(latent2)
-    #
-    # model = Model(inputs=[rna_input, atac_input], outputs=[out1, out2])
-    # model.compile(optimizer='adam', loss=None)
-    # model.add_loss(losses.mean_squared_error(rna_input, out1))
-    # model.add_loss(losses.mean_squared_error(atac_input, out2))
