e88ed2c79b53bd849f1782c8adedd4e04bfe2b8f
Jonas Simon Fleck <jonas.fleck@bsse.ethz.ch>
2020-11-04 10:12:28 +0100

    Better kernel functions.

--- latent/flow/utils.py
+++ latent/flow/utils.py
@@ -1,40 +1,39 @@
 import numpy as np
 import tensorflow as tf
 from keras import backend as K
+import tensorflow_probability as tfp
+kernels = tfp.math.psd_kernels
 
-# Kernel functions slightly modified from https://github.com/theislab/scarches
-def rbf_kernel(x, y):
-    dim = tf.cast(tf.shape(x)[1], tf.float32)
-    dist = squared_distance(x, y)
-    return tf.math.exp(-dist / dim)
-
-
+# Multi-scale RBF kernel modified from https://github.com/theislab/scarches
 def ms_rbf_kernel(x, y):
+    '''Multi-scale RBF kernel'''
     sigmas = [
         1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1,
         1, 5, 10, 15, 20, 25, 30, 35, 100,
         1e3, 1e4, 1e5, 1e6
     ]
-
     beta = 1. / (2. * (tf.expand_dims(sigmas, 1)))
     dist = squared_distance(x, y)
-    s = K.dot(beta, tf.reshape(dist, (1, -1)))
+    s = tf.tensordot(beta, tf.reshape(dist, (1, -1)), axes=0)
     return tf.reshape(tf.math.reduce_sum(tf.exp(-s), 0), tf.shape(dist)) / len(sigmas)
 
 
-def raphy_kernel(x, y, scales=[]):
-    dist = tf.expand_dims(squared_distance(x, y), 0)
-    scales = tf.expand_dims(tf.expand_dims(scales, -1), -1)
-    weights = tf.eval(tf.shape(scales)[0])
-    weights = tf.expand_dims(tf.expand_dims(weights, -1), -1)
-    kernel = weights * tf.math.exp(-dist / (tf.math.pow(scales, 2)))
-    return tf.math.reduce_sum(kernel, 0)
+def rbf_kernel(x, y):
+    '''Radial Basis Function or Exponentiated Quadratic kernel'''
+    kernel = kernels.ExponentiatedQuadratic()
+    return kernel.matrix(x, y)
+
+
+def rq_kernel(x, y):
+    '''Rational Quadratic kernel'''
+    kernel = kernels.RationalQuadratic()
+    return kernel.matrix(x, y)
 
 
 KERNELS = {
     'multiscale_rbf': ms_rbf_kernel,
     'rbf': rbf_kernel,
-    'raphy': raphy_kernel
+    'rq': rq_kernel
 }
 
 
