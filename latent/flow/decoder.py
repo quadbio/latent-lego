9e4a6ecbe0004694671af036d00d613a5450c336
Jonas Simon Fleck <jonas.fleck@bsse.ethz.ch>
2020-11-20 16:57:59 +0100

    Restructuring decoders: Type hints, decoder adds loss, delegates.

--- latent/flow/decoder.py
+++ latent/flow/decoder.py
@@ -7,24 +7,29 @@ from tensorflow.keras.regularizers import l1_l2
 import tensorflow.keras.layers as layers
 import tensorflow.keras.losses as losses
 
+from fastcore import delegates
+from typing import Iterable, Literal, Union, Callable
+
 from .activations import clipped_softplus, clipped_exp
 from .layers import ColwiseMult, DenseStack, SharedDispersion, Constant
+from .losses import NegativeBinomial, ZINB
 
 
 class Decoder(keras.Model):
     """Deocder base model"""
     def __init__(
         self,
-        x_dim,
-        name = 'decoder',
-        dropout_rate = 0.1,
-        batchnorm = True,
-        l1 = 0.,
-        l2 = 0.,
+        x_dim: int,
+        name: str = 'decoder',
+        dropout_rate: float = 0.1,
+        batchnorm: bool = True,
+        l1: float = 0.,
+        l2: float = 0.,
         hidden_units = [128, 128],
-        activation = 'leaky_relu',
-        initializer = 'glorot_normal',
-        **kwargs
+        activation: Union[str, Callable] = 'leaky_relu',
+        initializer: Union[str, Callable] = 'glorot_normal',
+        reconstruction_loss: Callable = None,
+        loss_name = 'rec_loss'
     ):
         super().__init__(name=name)
         self.x_dim = x_dim
@@ -35,6 +40,8 @@ class Decoder(keras.Model):
         self.activation = activation
         self.hidden_units =  hidden_units
         self.initializer = keras.initializers.get(initializer)
+        self.reconstruction_loss = reconstruction_loss
+        self.loss_name = loss_name
 
         # Define components
         if self.hidden_units:
@@ -57,50 +64,64 @@ class Decoder(keras.Model):
 
     def call(self, inputs):
         """Full forward pass through model"""
-        h = self.dense_stack(inputs) if self.hidden_units else inputs
-        h = self.final_layer(h)
-        outputs = self.final_act(h)
+        x, latent = inputs
+        h = self.hidden(latent)
+        output = self.final_layer(h)
+        self.add_reconstruction_loss(x, output)
         return outputs
 
-    def hidden(self, inputs):
+    def hidden(self, latent):
         """Pass through hidden layers"""
-        return self.hidden_layers(inputs) if self.hidden_units else inputs
+        return self.hidden_layers(latent) if self.hidden_units else latent
 
-    def output(self, inputs):
-        """Resonstructs output"""
-        return self.final_layer(inputs)
+    def add_reconstruction_loss(self, x, output):
+        """Adds reconstruction loss to final model loss"""
+        if self.reconstruction_loss:
+            rec_loss = self.reconstruction_loss(latent, output)
+            self.add_loss(rec_loss)
+            self.add_metric(rec_loss, name=self.loss_name)
 
 
-class CountDecoder(Decoder):
-    """
-    Count decoder model.
-    Rough reimplementation of the poisson Deep Count Autoencoder by Erslan et al. 2019
-    """
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+@delegates()
+class PoissonDecoder(Decoder):
+    """Decoder with poisson reconstruction loss."""
+    def __init__(
+        self,
+        name = 'poisson_decoder',
+        reconstruction_loss: Callable = losses.Poisson(),
+        loss_name: str = 'poisson_loss',
+        **kwargs
+    ):
+        super().__init__(name=name, **kwargs)
         # Define new components
         self.mean_layer = layers.Dense(
-            self.x_dim, name='mean',
+            self.x_dim,
+            name = 'mean',
             activation = clipped_exp,
             kernel_initializer = self.initializer
         )
-        self.norm_layer = ColwiseMult(name='reconstruction_output')
+        self.norm_layer = ColwiseMult(name='output')
 
     def call(self, inputs):
         """Full forward pass through model"""
-        h, sf = inputs
-        h = self.dense_stack(h)
+        x, latent, sf = inputs
+        h = self.hidden(h)
         mean = self.mean_layer(h)
         outputs = self.norm_layer([mean, sf])
+        self.add_reconstruction_loss(x, outputs)
         return outputs
 
 
-class NegativeBinomialDecoder(Decoder):
-    """
-    Negative Binomial decoder model.
-    Rough reimplementation of the NB Deep Count Autoencoder by Erslan et al. 2019
-    """
-    def __init__(self, dispersion='gene', **kwargs):
+@delegates()
+class NegativeBinomialDecoder(PoissonDecoder):
+    """Decoder with negative binomial reconstruction loss."""
+    def __init__(
+        self,
+        name: str = 'nb_decoder',
+        dispersion: Union[Literal['gene', 'cell-gene', 'constant'], float] = 'gene',
+        loss_name: str = 'nb_loss',
+        **kwargs
+    ):
         super().__init__(**kwargs)
         self.dispersion = dispersion
 
@@ -131,7 +152,7 @@ class NegativeBinomialDecoder(Decoder):
                 name = 'constant_dispersion',
                 activation = clipped_exp
             )
-        elif isinstance(dispersion, (float, int)):
+        elif isinstance(dispersion, float):
             self.dispersion_layer = Constant(
                 self.x_dim,
                 constant = self.dispersion,
@@ -143,34 +164,38 @@ class NegativeBinomialDecoder(Decoder):
 
     def call(self, inputs):
         """Full forward pass through model"""
-        h, sf = inputs
-        h = self.dense_stack(h)
+        x, latent, sf = inputs
+        h = self.hidden(latent)
         mean = self.mean_layer(h)
-        mean = self.norm_layer([mean, sf])
+        outputs = self.norm_layer([mean, sf])
         disp = self.dispersion_layer(h)
-        return [mean, disp]
+        self.reconstruction_loss = NegativeBinomial(theta=disp)
+        self.add_reconstruction_loss(x, outputs)
+        return outputs
 
 
+@delegates()
 class ZINBDecoder(NegativeBinomialDecoder):
-    """
-    ZINB decoder model.
-    Rough reimplementation of the ZINB Deep Count Autoencoder by Erslan et al. 2019
-    """
+    """Decoder with ZINB reconstruction loss"""
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        
         # Define new components
         self.pi_layer = layers.Dense(
-            self.x_dim, name='dispersion',
+            self.x_dim,
+            name = 'dropout_rate',
             activation = 'sigmoid',
             kernel_initializer = self.initializer
         )
 
     def call(self, inputs):
         """Full forward pass through model"""
-        h, sf = inputs
+        x, latent, sf = inputs
         h = self.dense_stack(h)
         mean = self.mean_layer(h)
-        mean = self.norm_layer([mean, sf])
+        outputs = self.norm_layer([mean, sf])
         disp = self.dispersion_layer(h)
         pi = self.pi_layer(h)
-        return [mean, disp, pi]
+        self.reconstruction_loss = ZINB(theta=disp, pi=pi)
+        self.add_reconstruction_loss(x, outputs)
+        return outputs
