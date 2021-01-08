26c84e4b0ca49df9ba8ade74573b7f73584ddc16
Jonas Simon Fleck <jonas.fleck@bsse.ethz.ch>
2021-01-08 11:26:45 +0100

    Argument arror if x_dim and decoder are unspecified.

--- latent/models/ae.py
+++ latent/models/ae.py
@@ -52,6 +52,8 @@ class Autoencoder(keras.Model):
         super().__init__(name=name)
         self.latent_dim = int(latent_dim)
         self.x_dim = int(x_dim) if x_dim else None
+        if not x_dim and not decoder:
+            raise ArgumentError('Either x_dim or decoder must be specified.')
         self.encoder_units = encoder_units
         self.decoder_units = decoder_units
         self.reconstruction_loss = reconstruction_loss
