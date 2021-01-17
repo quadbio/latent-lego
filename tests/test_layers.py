9ed09232d6e5bdf09330eb77a4de967fb209ba7f
joschif <jonas.simon.fleck@gmail.com>
2021-01-17 14:04:45 +0100

    More tests.

--- tests/test_layers.py
+++ tests/test_layers.py
@@ -6,7 +6,6 @@ from latent.layers import *
 nx = 600
 nd = 200
 X = np.random.uniform(low=0, high=30, size=(nx, nd)).astype(np.float32)
-sf = np.ones(nx)
 cond = np.random.randint(3, size=nx).astype(np.float32)
 ld = 20
 
@@ -25,21 +24,32 @@ def test_shared_dispersion():
 	sdisp = SharedDispersion(units)
 	res = sdisp(X).numpy()
 	assert res.shape == (nx, units)
+	assert np.all(np.all(res == res[0,:], axis = 0))
 
 
-def test_model_construction():
+def test_constant():
+	units = 2
+	x = np.array([[1,2,3], [1,2,3], [3,2,1]])
+	exp_res = np.array([[2,2], [2,2], [2,2]])
+	const = Constant(units, constant=2, activation='linear')
+	res = const(x).numpy()
+	assert np.allclose(res, exp_res)
+
+
+def test_sequential_api():
 	enc = keras.Sequential([
+		DenseBlock(units=512, activation='relu'),
 		DenseStack(hidden_units=[256,128,56], batchnorm=True, dropout_rate=0.2),
 		DenseBlock(units=20, activation='linear')
 	])
 	dec = keras.Sequential([
+		DenseBlock(units=20, activation='relu'),
 		DenseStack(hidden_units=[56,128,256], batchnorm=True, dropout_rate=0.1),
 		DenseBlock(units=200, activation='linear')
 	])
-	x_in = keras.Input(shape=(x_train.shape[1],))
-	sf_in = keras.Input(shape=(1,))
+	x_in = keras.Input(shape=(X.shape[1],))
 	x_latent = enc(x_in)
-	x_rec = decoder([x_in, x_latent, sf_in])
-	ae = keras.Model([x_in, sf_in], x_rec)
-	ae.compile(loss=None, optimizer='sgd')
-	ae.fit(x=[x_train, x_sf], y=x_train, epochs=10, batch_size=10)
+	x_rec = dec(x_latent)
+	ae = keras.Model(x_in, x_rec)
+	ae.compile(loss='mse', optimizer='sgd')
+	ae.fit(x=X, y=X, epochs=10, batch_size=10)
