412d1e60edd710bb8f2628f1a406851172174359
joschif <jonas.simon.fleck@gmail.com>
2021-09-19 22:56:33 +0200

    Implemented `predict` method.

--- latent/perturbation.py
+++ latent/perturbation.py
@@ -1,15 +1,18 @@
+import latent
 import numpy as np
-import tensorflow as tf
-import tensorflow_probability as tfp
+import scipy as sp
+from scipy.spatial.distance import pdist
 
+from typing import Iterable, Union
+from latent.utils import aggregate, to_dense
 
 # Perturbation prediction utils
 def test_train_split(
     adata,
     celltype_key,
-    condition_key,
+    predict_key,
     celltype_predict,
-    control_condition
+    control_name
 ):
     """Splits andata object into training and test set for pertrubation prediction. 
     The training set consists of controls of all cell types and stimulated state for all 
@@ -19,14 +22,14 @@ def test_train_split(
     Arguments:
         adata: An anndata object.
         celltype_predict: String indicating the metadata column containing celltype info.
-        condition_key: String indicating the metadata column containing condition info.
+        predict_key: String indicating the metadata column containing condition info.
         celltype_predict: A string or list of strings containing the celltype(s) 
             to be held out from training.
-        control_condition: A string or list of strings containing the control conditions.
+        control_name: A string or list of strings containing the control conditions.
     """
     ct_pred = np.array(celltype_predict)
-    ctrl_cond = np.array(control_condition)
-    stim_idx = ~adata.obs.loc[:, condition_key].isin(ctrl_cond)
+    ctrl_cond = np.array(control_name)
+    stim_idx = ~adata.obs.loc[:, predict_key].isin(ctrl_cond)
     ct_idx = adata.obs[celltype_key].isin(ct_pred)
     train = adata[~(stim_idx & ct_idx), :]
     test = adata[~(stim_idx & ct_idx), :]
@@ -39,29 +42,69 @@ class LatentVectorArithmetics:
     """
     def __init__(
         self,
-        model,
-        adata,
-        celltype_key,
-        celltype_predict,
-        condition_key,
-        control_condition,
-        weighted = False
+        model: latent.models.Autoencoder
     ):
         """
         Arguments:
-            model: A trained autoencoder model.
+            model: An autoencoder model.
+        """
+        self.model = model
+
+    def predict(
+        self, 
+        adata,
+        celltype_key: str,
+        celltype_predict: str,
+        predict_key: str,
+        control_name: Union[str, Iterable[str]],
+        condition_key: str = None,
+        weighted: bool = False
+    ):
+        """
+        Arguments:
             adata: An anndata object.
             celltype_key: String indicating the metadata column containing celltype info.
             celltype_predict: String indicating the cell type to predict.
-            condition_key: String indicating the metadata column containing 
-                condition info.
-            control_condition: String indicating the control condition.
+            predict_key: String indicating the metadata column containing 
+                the condition to predict info.
+            control_name: String indicating the control condition.
+            condition_key: String indicating the metadata column containing the 
+                condition info (used for conditional autoencoders)
             weighted: Whether to weight the latent vectors.
         """
-        self.model = model
-        self.adata = adata
-        self.celltype_key = celltype_key
-        self.celltype_predict = celltype_predict
-        self.condition_key = condition_key
-        self.control_condition = control_condition
-        self.weighted = weighted
+        ct_pred = np.array(celltype_predict)
+        ctrl_cond = np.array(control_name)
+        stim_idx = ~adata.obs.loc[:, predict_key].isin(ctrl_cond)
+        celltypes = adata.obs[celltype_key]
+        ct_idx = celltypes.isin(ct_pred)
+
+        stim_pred_from = adata[(~ct_idx & stim_idx), :]
+        ctrl_pred_from = adata[(~ct_idx & ~stim_idx), :]
+        pred_to = adata[(ct_idx & ~stim_idx), :]
+        
+        latent_stim = to_dense(self.model.transform(stim_pred_from.X))
+        latent_ctrl = to_dense(self.model.transform(ctrl_pred_from.X))
+        latent_pred_to = to_dense(self.model.transform(pred_to.X))
+
+        stim_mean = aggregate(
+            latent_stim, groups=celltypes[(~ct_idx & stim_idx)], axis=0)
+        ctrl_mean = aggregate(
+            latent_ctrl, groups=celltypes[(~ct_idx & ~stim_idx)], axis=0)
+
+        delta = stim_mean - ctrl_mean
+
+        if self.weighted:
+            weights = self._get_weights(
+                latent_ctrl, celltypes[(~ct_idx & ~stim_idx)])
+            mean_delta = np.mean(delta * weights)
+        else:
+            mean_delta = np.mean(delta)
+
+        return latent_pred_to + mean_delta
+
+
+    def _get_weights(self, latent, celltypes, metric='euclidean'):
+        latent_mean = aggregate(latent, celltypes, axis=0)
+        dists = sp.distance.pdist(latent_mean, metric=metric)
+        rev_dists = 1 / (dists + 1)
+        return rev_dists / np.sum(rev_dists)
