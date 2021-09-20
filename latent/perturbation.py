9093584f5ba12eaf6a66425c146ac980e82012f4
joschif <jonas.simon.fleck@gmail.com>
2021-09-20 09:35:55 +0200

    Changes `ctrl_name` to `pred_name`

--- latent/perturbation.py
+++ latent/perturbation.py
@@ -14,7 +14,7 @@ def test_train_split(
     celltype_key,
     predict_key,
     celltype_predict,
-    control_name
+    predict_name
 ):
     """Splits andata object into training and test set for pertrubation prediction. 
     The training set consists of controls of all cell types and stimulated state for all 
@@ -30,8 +30,8 @@ def test_train_split(
         control_name: A string or list of strings containing the control conditions.
     """
     ct_pred = np.array(celltype_predict)
-    ctrl_cond = np.array(control_name)
-    stim_idx = ~adata.obs.loc[:, predict_key].isin(ctrl_cond)
+    pred_cond = np.array(predict_name)
+    stim_idx = adata.obs.loc[:, predict_key].isin(pred_cond)
     ct_idx = adata.obs[celltype_key].isin(ct_pred)
     train = adata[~(stim_idx & ct_idx), :]
     test = adata[~(stim_idx & ct_idx), :]
@@ -60,7 +60,7 @@ class LatentVectorArithmetics:
         celltype_key: str,
         celltype_predict: str,
         predict_key: str,
-        control_name: Union[str, Iterable[str]],
+        pred_name: Union[str, Iterable[str]],
         condition_key: str = None,
         size_factor_key: str = None,
         weighted: bool = False
@@ -72,7 +72,7 @@ class LatentVectorArithmetics:
             celltype_predict: String indicating the cell type to predict.
             predict_key: String indicating the metadata column containing 
                 the condition to predict info.
-            control_name: String indicating the control condition.
+            pred_name: String indicating the condition to predict.
             condition_key: String indicating the metadata column containing the 
                 condition info (used for conditional autoencoders)
             size_factor_key: String indicating the metadata column containing the 
@@ -80,8 +80,8 @@ class LatentVectorArithmetics:
             weighted: Whether to weight the latent vectors.
         """
         ct_pred = np.array(celltype_predict)
-        ctrl_cond = np.array(control_name)
-        stim_idx = ~adata.obs.loc[:, predict_key].isin(ctrl_cond)
+        pred_cond = np.array(pred_name)
+        stim_idx = adata.obs.loc[:, predict_key].isin(pred_cond)
         celltypes = adata.obs[celltype_key]
         ct_idx = celltypes.isin(ct_pred)
 
